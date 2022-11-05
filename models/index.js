604a76f08280c244b78d25329af9a280a67ea594
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-11-04 19:30:11 -0700

    enable users to vote on posts

--- models/index.js
+++ models/index.js
@@ -1,8 +1,19 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
+	foreignKey: 'user_id',
+});
+
+User.belongsToMany(Post, {
+	through: Vote,
+	as: 'voted_posts',
+	foreignKey: 'user_id',
+});
+
+User.hasMany(Vote, {
 	foreignKey: 'user_id',
 });
 
@@ -10,4 +21,22 @@ Post.belongsTo(User, {
 	foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+	through: Vote,
+	as: 'voted_posts',
+	foreignKey: 'post_id',
+});
+
+Post.hasMany(Vote, {
+	foreignKey: 'post_id',
+});
+
+Vote.belongsTo(User, {
+	foreignKey: 'user_id',
+});
+
+Vote.belongsTo(Post, {
+	foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Vote };
